3df078b6a22b9ea06b0cc0c2db0ff55066286fd1
ryananjasmara <ryan.anjasmara@ralali.com>
2019-12-08 01:04:47 +0700

    add: major changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import "./assets/css/styles.css";
 import Pokedex from "./components/Pokedex";
 
 function App() {
-  return <Pokedex />;
+  return (
+    <div className="app">
+      <Pokedex />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/PokemonsCard/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { ButtonGroup, Button } from "reactstrap";
+
+export default function ListPokemon() {
+  return (
+    <div>
+      <h1 className="primary">Hello World</h1>
+    </div>
+  );
+}
