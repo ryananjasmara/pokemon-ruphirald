21063badf7649a423449acbfa54a9319e1525c34
ryananjasmara <ryan.anjasmara@ralali.com>
2019-12-10 19:25:04 +0700

    add: header & footer

---
+++ src/components/Templates/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Button, Spinner } from "reactstrap";
+import IfComponent from "../../lib/IfComponent";
+
+function Footer({ isFetching, clickHandle, dataCount }) {
+  return (
+    <div className="footer">
+      <IfComponent
+        ifStatement={!isFetching}
+        thenRender={
+          <Button
+            color="success"
+            onClick={() => {
+              clickHandle(dataCount + 10);
+            }}
+          >
+            Load More
+          </Button>
+        }
+        otherRender={<Spinner color="success" />}
+      />
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Templates/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>
+        <font className="header-text">Pokémon&nbsp;</font>
+        <font className="text-ruby">Ru</font>
+        <font className="text-sapphire">phi</font>
+        <font className="text-emerald">rald</font>
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
