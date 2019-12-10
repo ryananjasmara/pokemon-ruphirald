4c453cdf0360c2030e50abd46ac03fe139f44fe3
ryananjasmara <ryan.anjasmara@ralali.com>
2019-12-10 19:29:26 +0700

    finished pokedex list

--- src/components/Pokedex/index.js
+++ src/components/Pokedex/index.js
@@ -1,52 +1,118 @@
-import React, { useState } from "react";
-import {
-  Button,
-  ButtonGroup,
-  Card,
-  CardImg,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-  CardText
-} from "reactstrap";
-import Header from "../Header";
+import React, { useState, useEffect } from "react";
+import { Row, Col, Container, Button } from "reactstrap";
+import axios from "axios";
+import { capitalizeFirstLetter } from "../../utils/StringManipulation";
+import Header from "../Templates/Header";
+import Footer from "../Templates/Footer";
 import NavigationsTab from "../NavigationsTab";
+import PokemonsCard from "../PokemonsCard";
+import PokemonsDetail from "../PokemonsDetail";
+
+const styles = {
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  cardContainer: {
+    justifyContent: "space-evenly",
+    marginBottom: 25
+  },
+  floatingButton: {
+    borderRadius: 1000,
+    position: "fixed",
+    right: 15,
+    bottom: 15
+  }
+};
 
 function Pokedex() {
   const [currentTab, setCurrentTab] = useState(0);
+  const [currentData, setCurrentData] = useState(0);
+  const [pokemonData, setPokemonData] = useState("");
+  const [isFetching, setIsFetching] = useState(false);
+  const [movesData, setMovesData] = useState("");
+
+  useEffect(() => {
+    getData();
+  }, [currentData]);
 
   function handleChangeTab(value) {
     setCurrentTab(value);
-    console.log("jalan");
   }
 
-  return (
-    <div className="container">
-      <Header />
-      <NavigationsTab currentTab={currentTab} clickHandle={handleChangeTab} />
+  function handleLoadMore(value) {
+    setCurrentData(value);
+  }
 
-      {/* Card */}
-      <div className="card">
-        <Card>
-          <CardImg
-            top
-            width="100%"
-            src="/assets/318x180.svg"
-            alt="Card image cap"
-          />
-          <CardBody>
-            <CardTitle>Card title</CardTitle>
-            <CardSubtitle>Card subtitle</CardSubtitle>
-            <CardText>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </CardText>
-            <Button>Button</Button>
-          </CardBody>
-        </Card>
-      </div>
-      {/* End Card */}
-    </div>
+  const handleBackToTop = () => {
+    window.scroll({ top: 0, left: 0, behavior: "smooth" });
+  };
+
+  function getData() {
+    const offset = currentData;
+    const limit = 10;
+
+    setIsFetching(true);
+
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`)
+      .then(res => {
+        setPokemonData([...pokemonData, ...res.data.results]);
+        setIsFetching(false);
+      });
+  }
+
+  // Get All Moves Data
+  function loadMoves() {
+    // Total moves data length is 726
+    // get by trial and error hit at the API
+    // well, i know its static
+    // but after looking at the API
+    // this might be better choice.
+    // for (let x = 1; x <= 726; x++) {
+    //   axios.get(`https://pokeapi.co/api/v2/pokemon/move/${x}`).then(res => {
+    //     setMovesData([...movesData, ...res.data.results]);
+    //   });
+    // }
+  }
+
+  loadMoves();
+
+  return (
+    <Container style={styles.container}>
+      <Col>
+        {/* Floating Button */}
+        <Button
+          color="success"
+          className="button-float"
+          onClick={handleBackToTop}
+          style={styles.floatingButton}
+        >
+          <img src={require("../../assets/images/arrow_up.png")} />
+        </Button>
+        {/* Apps Title & Navigation */}
+        <Header />
+        <NavigationsTab currentTab={currentTab} clickHandle={handleChangeTab} />
+        {/* Pokemons Card List */}
+        <Row style={styles.cardContainer}>
+          {pokemonData &&
+            pokemonData.map((item, index) => {
+              const pokemonName = capitalizeFirstLetter(item.name);
+              return (
+                <PokemonsCard index={index + 1} pokemonName={pokemonName} />
+              );
+            })}
+        </Row>
+        {/* <PokemonsDetail /> */}
+        {/* Load More Button, etc */}
+        <Footer
+          isFetching={isFetching}
+          clickHandle={handleLoadMore}
+          dataCount={currentData}
+        />
+      </Col>
+    </Container>
   );
 }
 
--- src/components/PokemonsCard/index.js
+++ src/components/PokemonsCard/index.js
@@ -1,10 +1,47 @@
-import React from "react";
-import { ButtonGroup, Button } from "reactstrap";
+import React, { useState } from "react";
+import { Badge, Card, CardBody, CardTitle, CardSubtitle } from "reactstrap";
+import { pokedexNumberFormat } from "../../utils/StringManipulation";
 
-export default function ListPokemon() {
+const styles = {
+  touched: {
+    opacity: 0.75
+  },
+  notTouched: {
+    opacity: 1
+  }
+};
+
+function PokemonsCard({ index, pokemonName }) {
+  const [isTouched, setIsTouched] = useState(false);
+  const pokemonImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index}.png`;
+
+  // Mouse Down
+  const handleTouched = () => {
+    setIsTouched(!isTouched);
+  };
+
+  // Mouse Up
+  const handleRevert = () => {
+    setTimeout(() => {
+      setIsTouched(false);
+    }, 150);
+  };
+
   return (
-    <div>
-      <h1 className="primary">Hello World</h1>
-    </div>
+    <Card
+      style={isTouched ? styles.touched : styles.notTouched}
+      onMouseUp={handleRevert}
+      onMouseDown={handleTouched}
+    >
+      <CardBody>
+        <CardTitle>
+          <Badge color="dark">{pokedexNumberFormat(index)}</Badge>
+        </CardTitle>
+        <CardSubtitle>{pokemonName}</CardSubtitle>
+      </CardBody>
+      <img width="200" src={pokemonImage} alt="" />
+    </Card>
   );
 }
+
+export default PokemonsCard;
