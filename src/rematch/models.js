c7b30fef19898028b494fbbbca9115cec60beb08
Ryan <ryan.anjasmara@ralali.com>
2020-02-26 23:32:00 +0700

    chore: register PokemonListModel

--- src/rematch/models.js
+++ src/rematch/models.js
@@ -1,5 +1,7 @@
 import MyPokemonModels from "../models/MyPokemonModels";
+import PokemonListModel from "../models/PokemonListModel";
 
 export default {
-  MyPokemonModels
+  MyPokemonModels,
+  PokemonListModel
 };
